a08e2d6e977078e8ed2d2b26ab0dd636972c497b
vitaligever <supultura12@gmail.com>
2016-11-20 14:49:46 +0200

    added graph and graph test file

--- Matala1-graph/src/graph/Main.java
+++ Matala1-graph/src/graph/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		@SuppressWarnings("unused")
-		OutputGraph Graph=new OutputGraph("G5.txt", "G5test.txt", "G0outPut.txt");
+		OutputGraph Graph=new OutputGraph("Graph1.txt", "testGraph1.txt", "G0outPut.txt");
 	
 	}
 }
